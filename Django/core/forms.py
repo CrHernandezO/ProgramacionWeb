ed7c51c96a743a7f66363c7e117775763b54c6dd
Happy <cr.hernandezo@duocuc.cl>
2024-07-05 17:58:39 -0400

    base de datos implementada

--- Django/core/forms.py
+++ Django/core/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Genero, Usuario
+from .models import Genero, Usuario,Anime,Marca
 
 from django.forms import ModelForm
 
@@ -11,4 +11,14 @@ class GeneroForm(ModelForm):
 class UsuarioForm(ModelForm):
     class Meta:
         model = Usuario
+        fields = "__all__"
+
+class AnimeForm(ModelForm):
+    class Meta:
+        model = Anime
+        fields = "__all__"
+
+class MarcaForm(ModelForm):
+    class Meta:
+        model = Marca
         fields = "__all__"
--- Django/core/views.py
+++ Django/core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .models import Genero, Usuario
-from .forms import GeneroForm,UsuarioForm
+from .models import Genero, Usuario, Anime,Marca,Figura
+from .forms import GeneroForm,UsuarioForm,AnimeForm,MarcaForm
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 
@@ -41,6 +41,7 @@ def somos(request):
 
     context={}
     return render(request,"pages/somos.html",context)
+
 @login_required
 def crud(request):
     usuarios = Usuario.objects.all()
@@ -181,6 +182,141 @@ def user_update(request):
         return render(request, "pages/user_update.html", context)
     
 @login_required
+def crud_figura(request):
+    usuarios = Usuario.objects.all()
+    context = {
+        "usuarios": usuarios,
+    }
+    return render(request, "pages/crud.html", context)
+
+
+def figura_add(request):
+    if request.method != "POST":
+        figuras = Figura.objects.all()
+        context = {
+            "figuras": figuras,
+        }
+        return render(request, "pages/figura_add.html", context)
+    else:
+        id_figura = request.POST["idfigura"]
+        nombre_figura = request.POST["nombre"]
+        anime = request.POST["anime"]
+        marca = request.POST["marca"]
+        fecha_lanzamiento = request.POST["fecha"]
+        precio = request.POST["precio"]
+        tamano = request.POST["tamano"]
+
+
+        objAnime = Anime.objects.get(id_anime=anime)
+        objMarca= Marca.objects.get(id_marca=marca)
+
+        obj = Usuario.objects.create(
+            id_figura=id_figura,
+            nombre_figura=nombre_figura,
+            anime=objAnime,
+            marca=objMarca,
+            fecha_lanzamiento=fecha_lanzamiento,
+            precio=precio,
+            tamano=tamano,
+ 
+        )
+        obj.save()
+        context = {
+            "mensaje": "Registro Exitoso",
+        }
+        return render(request, "pages/figura_add.html", context)
+
+
+def figura_del(request, pk):
+    try:
+        figura = Figura.objects.get(id_figura=pk)
+        figura.delete()
+
+        figuras = Figura.objects.all()
+        context = {
+            "mensaje": "Figura Eliminada",
+            "figuras": figuras,
+        }
+        return render(request, "pages/crud_figura.html", context)
+    except:
+        figuras = Figura.objects.all()
+        context = {
+            "mensaje": "Error,Id_Figura no encontrado...",
+            "figuras": figuras,
+        }
+        return render(request, "pages/crud_figura.html", context)
+
+def figura_findEdit(request,pk):
+    if pk!="":
+        """ 
+            objects.get() = Obtener datos con filtro
+            objects.all() = Obtener todos
+        """
+        figura = Figura.objects.get(id_figura=pk)
+        marcas = Marca.objects.all()
+        animes = Anime.objects.all()
+
+        context={
+            "figura":figura,
+            "marcas":marcas,
+            "animes":animes,
+
+        }
+        return render(request,"pages/figura_update.html",context)
+    else:
+        figuras = Figura.objects.all()
+        context={
+            "mensaje":"Error,Rut no encontrado",
+            "figuras":figuras
+        }
+        return render(request,"pages/crud_figura.html",context)
+
+def figura_update(request):
+    if request.method=="POST":
+        """ 
+            Capturo todos los datos del front
+            Identificamos
+            Asignamos nombre 
+        """
+        id_figura = request.POST["idfigura"]
+        nombre_figura = request.POST["nombre"]
+        anime = request.POST["anime"]
+        marca = request.POST["marca"]
+        fecha_lanzamiento = request.POST["fecha"]
+        precio = request.POST["precio"]
+        tamano = request.POST["tamano"]
+
+
+        objAnime = Anime.objects.get(id_anime=anime)
+        objMarca= Marca.objects.get(id_marca=marca)
+
+        obj = Usuario.objects.create(
+            id_igura=id_figura,
+            nombre_figura=nombre_figura,
+            anime=objAnime,
+            marca=objMarca,
+            fecha_lanzamiento=fecha_lanzamiento,
+            precio=precio,
+            tamano=tamano,
+ 
+        )
+        obj.save()
+
+        animes = Anime.objects.all()
+        marcas = Marca.objects.all()
+
+        context = {
+            "mensaje": "Modificado con Exito",
+            "animes":animes,
+            "marcas":marcas,
+
+            "figura":obj,
+        }
+        return render(request, "pages/figura_update.html", context)
+    
+
+
+@login_required
 def crud_genero(request):
     generos = Genero.objects.all()
 
@@ -255,6 +391,157 @@ def genero_edit(request,pk):
             "generos":generos
         }
         return render(request,"pages/crud_genero.html",context)
+@login_required
+def crud_anime(request):
+    animes = Anime.objects.all()
+
+    context={
+        "animes":animes,
+    }
+    return render(request,"pages/crud_anime.html",context)
+
+def anime_add(request):
+    formAnime = AnimeForm()
+    formUsuario = UsuarioForm()
+    if request.method=="POST":
+        nuevo = AnimeForm(request.POST)
+        if nuevo.is_valid():
+            nuevo.save()
+
+            context={
+                "mensaje":"Agregado con exito",
+                "form":formAnime
+            }
+            return render(request,"pages/anime_add.html",context)
+    else:
+        context = {
+            "form":formAnime,
+            "form2":formUsuario
+        }
+        return render(request,"pages/anime_add.html",context)
+
+def anime_del(request,pk):
+    try:
+        anime = Anime.objects.get(id_anime=pk)
+        anime.delete()
+
+        animes = Anime.objects.all()
+        context={
+            "mensaje":"Registro eliminado exitosamente",
+            "animes":animes
+        }
+        return render(request,"pages/crud_anime.html",context)
+    except:
+        animes = Anime.objects.all()
+        context={
+            "mensaje":"Error, Anime no encontrado...",
+            "animes":animes
+        }
+        return render(request,"pages/crud_anime.html",context)
+
+def anime_edit(request,pk):
+    if pk!="":
+        anime = Anime.objects.get(id_anime=pk)
+        form = AnimeForm(instance=anime)
+        if request.method=="POST":
+            nuevo = AnimeForm(request.POST,instance=anime)
+
+            if nuevo.is_valid():
+                nuevo.save()
+
+                context ={
+                    "mensaje":"Modificado con exito",
+                    "form":nuevo
+                }
+                return render(request,"pages/anime_edit.html",context)
+        else:
+            context={
+                "form":form,
+            }
+            return render(request,"pages/anime_edit.html",context)
+    else:
+        animes = Anime.objects.all()
+        context={
+            "mensaje":"Error, anime no encontrado",
+            "animes":animes
+        }
+        return render(request,"pages/crud_anime.html",context)
+
+@login_required
+def crud_marca(request):
+    marcas = Marca.objects.all()
+
+    context={
+        "marcas":marcas,
+    }
+    return render(request,"pages/crud_marca.html",context)
+
+def marca_add(request):
+    formMarca = MarcaForm()
+    formUsuario = UsuarioForm()
+    if request.method=="POST":
+        nuevo = MarcaForm(request.POST)
+        if nuevo.is_valid():
+            nuevo.save()
+
+            context={
+                "mensaje":"Agregado con exito",
+                "form":formMarca
+            }
+            return render(request,"pages/marca_add.html",context)
+    else:
+        context = {
+            "form":formMarca,
+            "form2":formUsuario
+        }
+        return render(request,"pages/marca_add.html",context)
+
+def marca_del(request,pk):
+    try:
+        marca = Marca.objects.get(id_marca=pk)
+        marca.delete()
+
+        marcas = Marca.objects.all()
+        context={
+            "mensaje":"Registro eliminado exitosamente",
+            "marcas":marcas
+        }
+        return render(request,"pages/crud_marca.html",context)
+    except:
+        marcas = Marca.objects.all()
+        context={
+            "mensaje":"Error, Marca no encontrado...",
+            "marcas":marcas
+        }
+        return render(request,"pages/crud_marca.html",context)
+
+def marca_edit(request,pk):
+    if pk!="":
+        marca = Marca.objects.get(id_marca=pk)
+        form = MarcaForm(instance=marca)
+        if request.method=="POST":
+            nuevo = MarcaForm(request.POST,instance=marca)
+
+            if nuevo.is_valid():
+                nuevo.save()
+
+                context ={
+                    "mensaje":"Modificado con exito",
+                    "form":nuevo
+                }
+                return render(request,"pages/marca_edit.html",context)
+        else:
+            context={
+                "form":form,
+            }
+            return render(request,"pages/marca_edit.html",context)
+    else:
+        marcas = Marca.objects.all()
+        context={
+            "mensaje":"Error, marca no encontrado",
+            "marcas":marcas
+        }
+        return render(request,"pages/crud_marca.html",context)
 
 def loginSession(request):
     if request.method=="POST":
